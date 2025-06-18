f8e09659496c255b86a376e2b9b3a8f9ecb15b93
Sachee <sacheeghimire@gmail.com>
2025-06-18 20:58:09 +0545

    creating transporter

--- utils/email.js
+++ utils/email.js
@@ -17,6 +17,21 @@ const sendEmail = async (options) => {
       logger: true,
       debug: true
     });
+      // Prepare email content based on template type
+    let emailContent;
+    
+    if (options.template === 'passwordReset') {
+      emailContent = emailTemplates.passwordReset(options.name, options.code);
+    } else if (options.template === 'merchantPasswordReset') {
+      emailContent = emailTemplates.merchantPasswordReset(options.ownerName, options.businessName, options.code);
+    } else {
+      // Fallback to custom or generic template
+      emailContent = {
+        subject: options.subject || 'Notification from Tapyze',
+        text: options.message,
+        html: options.html || emailTemplates.generic(options.message).html
+      };
+    }
 
 
 export { sendEmail };
